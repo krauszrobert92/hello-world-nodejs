e23858b729c9e7bd89499ef797cfe85d9b76abf0
Róbert Krausz <krauszrobert92@gmail.com>
2025-01-17 14:12:07 +0100

    feat(server): update root route to return Hello World message

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require('path')
 const app = express()
 
 app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    // res.sendFile(path.join(__dirname, '/index.html'));
+
+    console.log(`Hello World Application is running on port ${process.env.PORT}`)
+    res.send("Hello World Application");
+    res.status(200).end();
+
 })
 
 app.listen(process.env.PORT, () => {
